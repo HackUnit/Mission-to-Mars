753cc0a8630c229459b5a584503c1c4fd81636a2
HackUnit <j.larose26@gmail.com>
2022-08-15 16:55:11 -0500

    Updated scraping.py

--- scraping.py
+++ scraping.py
@@ -1,21 +1,50 @@
 # Import Splinter, BeautifulSoup, and Pandas
 import pandas as pd
 
+import datetime as dt
+
 from splinter import Browser
 
 from bs4 import BeautifulSoup as soup
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-from webbrowser import Chrome
+#from webbrowser import Chrome
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+
+    data = {
+
+        "news_title": news_title,
+
+        "news_paragraph": news_paragraph,
+        
+        "featured_image": featured_image(browser),
+        
+        "facts": mars_facts(),
+        
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+
+    return data
+
 
 
 def mars_news(browser):
 
+    # Scrape Mars News
     # Visit the mars nasa news site
     url = 'https://redplanetscience.com'
     browser.visit(url)
@@ -67,6 +96,8 @@ def featured_image(browser):
     html = browser.html
     img_soup = soup(html, 'html.parser')
 
+
+    # Add try/except for error handling
     try:
 
         # Find the relative image url
@@ -87,6 +118,7 @@ def featured_image(browser):
 
 def mars_facts():
 
+    # Add try/except for error handling
     try:
 
         # use 'read_html' to scrape the facts table into a dataframe
@@ -96,11 +128,16 @@ def mars_facts():
         return None
 
     # Assign columns and set index of dataframe
-    df.columns=['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
 
 
-    #browser.quit()
+
+if __name__ == "__main__":
+
+    # If running as a script, print scraped data
+
+    print(scrape_all())
