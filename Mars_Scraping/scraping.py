4726d8794276a03d1864f2991d6c12676ad724a2
HackUnit <j.larose26@gmail.com>
2022-08-18 23:44:55 -0500

    Modified scraping.py

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -25,7 +25,7 @@ def scrape_all():
 
     executable_path = {'executable_path': ChromeDriverManager().install()}
     # Initiate headless driver for deployment
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
     news_title, news_paragraph = mars_news(browser)
     # Run all scraping functions and store results in a dictionary
     data = {
