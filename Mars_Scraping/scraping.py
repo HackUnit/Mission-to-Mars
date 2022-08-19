1a2ac9a863ec7e32a36334f644cc2937e33c0ab0
HackUnit <j.larose26@gmail.com>
2022-08-18 23:06:50 -0500

    Modified Scraping.py

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -1,4 +1,6 @@
+
 # Import Splinter, BeautifulSoup, and Pandas
+
 import pandas as pd
 
 import datetime as dt
@@ -9,6 +11,8 @@ from bs4 import BeautifulSoup as soup
 
 from webdriver_manager.chrome import ChromeDriverManager
 
+#from selenium import webdriver
+
 #from webbrowser import Chrome
 
 manager = ChromeDriverManager()
@@ -16,38 +20,26 @@ manager.install()
 
 
 
+# ## Scrape All
 def scrape_all():
 
-    # Initiate headless driver for deployment
-
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
-
-    # Set variables for mars_news
+    # Initiate headless driver for deployment
+    browser = Browser('chrome', **executable_path, headless=False)
     news_title, news_paragraph = mars_news(browser)
-    hemisphere_image_urls=hemisphere(browser)
-    # Run all scraping functions and store results in dictionary
-
+    # Run all scraping functions and store results in a dictionary
     data = {
-
         "news_title": news_title,
-
         "news_paragraph": news_paragraph,
-        
         "featured_image": featured_image(browser),
-        
         "facts": mars_facts(),
-        
-        "hemispheres": hemisphere_image_urls,
+        "last_modified": dt.datetime.now(),
+        "hemisphere_data": hemisphere_scrape(browser)
 
-        "last_modified": dt.datetime.now()
     }
-
     # Stop webdriver and return data
     browser.quit()
-
     return data
-
 
 
 def mars_news(browser):
@@ -143,33 +135,42 @@ def mars_facts():
     return df.to_html(classes="table table-striped")
 
 
-def hemisphere(browser):
-
-    url = 'https://marshemispheres.com/'
-
+#----------------Mars Hemisphere Scraping------------------------------
+def hemisphere_scrape(browser) :
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-
-
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # Parse the html with beautifulsoup
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
 
-    imgs_links = browser.find_by_css("a.product-item h3")
+    # Get the links for each of the 4 hemispheres
+    hemi_links = hemi_soup.find_all('h3')
+    # hemi_links
 
-    for i in range(4):
-        hemispheres = {}
-        browser.find_by_css('a.product-item h3')[i].click()
-        element = browser.links.find_by_text('Sample').first
-        img_url = element['href']
-        title = browser.find_by_css('h2.title').text
-        hemispheres['img_url'] = img_url
-        hemispheres['title'] = title
-        hemisphere_image_urls.append(hemispheres)
+    # loop through each hemisphere link
+    for hemi in hemi_links:
+        # Navigate and click the link of the hemisphere
+        img_page = browser.find_by_text(hemi.text)
+        img_page.click()
+        html= browser.html
+        img_soup = soup(html, 'html.parser')
+        # Scrape the image link
+        img_url = 'https://astrogeology.usgs.gov/' + str(img_soup.find('img', class_='wide-image')['src'])
+        # Scrape the title
+        title = img_soup.find('h2', class_='title').text
+        # Define and append to the dictionary
+        hemisphere = {'img_url': img_url,'title': title}
+        hemisphere_image_urls.append(hemisphere)
         browser.back()
-
+        # print(hemisphere_image_urls)
     return hemisphere_image_urls
 
-
+#----------------Ending Code ------------------------------------------
 if __name__ == "__main__":
-
-    # If running as a script, print scraped data
-
+    # If running as script, print scraped data
     print(scrape_all())
